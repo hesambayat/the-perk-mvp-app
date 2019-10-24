88360c0f4b78be8c699516f3c32a0e4b01ee0f9b
Hesam Bayat <hesam.bayat@gmail.com>
2019-10-24 19:46:10 +0400

    Init

---
+++ src/style/index.js
@@ -0,0 +1,11 @@
+// Normalize
+import './normalize.css'
+//
+import './base.css'
+import './layout.css'
+import './typography.css'
+import './forms.css'
+import './alignments.css'
+import './spacing.css'
+import './accessibility.css'
+import './components.css'
