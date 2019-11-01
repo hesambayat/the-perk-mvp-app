b1a7eb0688e96e312fadd0ad60c6b654a09104b0
Hesam Bayat <hesam.bayat@gmail.com>
2019-11-01 20:45:56 +0400

    Bundle authorization on requests

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -1,9 +1,11 @@
 import { split } from 'apollo-link'
-import { HttpLink } from 'apollo-link-http'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import { ApolloClient } from 'apollo-client'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { Storage } from '../lib'
 
 const wsLink = new WebSocketLink({
   uri: process.env.REACT_APP_WS_ENDPOINT,
@@ -12,8 +14,20 @@ const wsLink = new WebSocketLink({
   },
 })
 
-const httpLink = new HttpLink({
-  uri: process.env.REACT_APP_HTTP_ENDPOINT,
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_HTTP_ENDPOINT
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = Storage.login.token
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
 })
 
 const link = split(
@@ -26,7 +40,7 @@ const link = split(
     )
   },
   wsLink,
-  httpLink,
+  authLink.concat(httpLink),
 )
 
 export default new ApolloClient({
